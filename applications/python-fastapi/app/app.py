0dff4a777ccae6a9c43db52c2826f770c7a0be93
DefCon-007 <ayushgoyal.iitkgp@gmail.com>
2023-07-07 19:38:34 +0100

    Add database connection and get/post endpoint for fastapi server

--- applications/python-fastapi/app/app.py
+++ applications/python-fastapi/app/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-
+from sqlmodel import Session
+from db import engine
+from models import Todo
 app = FastAPI()
 
 
@@ -7,3 +9,20 @@ app = FastAPI()
 def hello_world():
     return {"message": "OK"}
 
+@app.post("/api/v1/add-todo")
+def add_todo(todo: Todo):
+    print('Adding todo', todo)
+
+    with Session(engine) as session:
+        session.add(todo)
+        session.commit()
+
+    return {"message": "OK"}
+
+
+@app.get("/api/v1/get-all-todos")
+def get_all_todos():
+    with Session(engine) as session:
+        todos = session.query(Todo).all()
+
+    return todos
--- applications/python-fastapi/app/server.py
+++ applications/python-fastapi/app/server.py
@@ -1,4 +1,9 @@
 import uvicorn
+from db import create_db_and_tables
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    create_db_and_tables()
+    port = int(os.environ.get('PORT', 8000))
+
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
